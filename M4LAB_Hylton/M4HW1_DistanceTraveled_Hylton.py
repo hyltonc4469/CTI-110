a2ea94acb8f9af69644bec76c697d82f5a48905d
hyltonc4469 <33227451+hyltonc4469@users.noreply.github.com>
2017-11-10 12:35:12 -0500

    Add files via upload
    M4Lab with Distance Traveled assignment.  Tip and Tax Assignment not yet attached.

---
+++ M4LAB_Hylton/M4HW1_DistanceTraveled_Hylton.py
@@ -0,0 +1,25 @@
+#  CTI-110
+#  M4HW1-Distance Traveled
+#  Marie Hylton
+#  10 November 2017
+#
+
+
+# This is a program
+# that specifies the distance
+# a car will travel
+# when traveling at the speed
+# of 70 miles per hour
+
+
+
+#Calculate the distance traveled after x hours.
+speed=(70)
+distanceafter6 = int(speed *6)
+distanceafter10= int(speed*10)
+distanceafter15= int(speed*15)
+
+print('The car will travel a distance of', distanceafter6,'miles after 6 hours.')
+print('The car will travel a distance of', distanceafter10,'miles after 10 hours.')
+print('The car will travel a distance of', distanceafter15,'miles after 15 hours.')
+
